2d1ac23e79bbc878d0df717f77deeb32d195ec1d
KD8BXP <kd8bxp@aol.com>
2017-08-25 10:44:35 -0400

    changed beacon sktech for arduino-LoRa library and ESP32 board

--- arduino/beacon/beacon.ino
+++ arduino/beacon/beacon.ino
@@ -1,67 +1,39 @@
 /* This is a beacon for the LoRaHam protocol by KK4VCZ.
  * https://github.com/travisgoodspeed/loraham/
  * 
+ * Modified by LeRoy Miller, KD8BXP
+ * Aug 25, 2017 For use with the TTGO WIFI_LoRa_32 Boards
+ * https://www.aliexpress.com/item/TTGO-loraSX1278-ESP32-0-96OLED-16-Mt-bytes-128-Mt-bit-433Mhz-for-arduino/32824758955.html
+ * 
+ * Significant changes:
+ * 
+ * Library Changed to: arduino-LoRa
+ * https://github.com/sandeepmistry/arduino-LoRa
+ * 
  */
 
-#define CALLSIGN "KK4VCZ-17"
-#define COMMENTS "2600mAh AVR32"
+#define CALLSIGN "KD8BXP-10"
+#define COMMENTS "2600mAh ESP32"
  
 #include <SPI.h>
-#include <RH_RF95.h>  //See http://www.airspayce.com/mikem/arduino/RadioHead/
- 
-/* for feather32u4  */
-#define RFM95_CS 8
-#define RFM95_RST 4
-#define RFM95_INT 7
-#define VBATPIN A9
+#include <LoRa.h> //https://github.com/sandeepmistry/arduino-LoRa
 
-/* for feather m0  
-#define RFM95_CS 8
-#define RFM95_RST 4
-#define RFM95_INT 3
-#define VBATPIN A7
-*/
- 
-/* for shield 
-#define RFM95_CS 10
-#define RFM95_RST 9
-#define RFM95_INT 7
-*/
- 
- 
-/* for ESP w/featherwing 
-#define RFM95_CS  2    // "E"
-#define RFM95_RST 16   // "D"
-#define RFM95_INT 15   // "B"
-*/
- 
-/* Feather 32u4 w/wing
-#define RFM95_RST     11   // "A"
-#define RFM95_CS      10   // "B"
-#define RFM95_INT     2    // "SDA" (only SDA/SCL/RX/TX have IRQ!)
-*/
- 
-/* Feather m0 w/wing 
-#define RFM95_RST     11   // "A"
-#define RFM95_CS      10   // "B"
-#define RFM95_INT     6    // "D"
-*/
- 
-/* Teensy 3.x w/wing 
-#define RFM95_RST     9   // "A"
-#define RFM95_CS      10   // "B"
-#define RFM95_INT     4    // "C"
-*/
+// WIFI_LoRa_32 ports
 
- 
-// Change to 434.0 or other frequency, must match RX's freq!
-#define RF95_FREQ 434.0
- 
-// Singleton instance of the radio driver
-RH_RF95 rf95(RFM95_CS, RFM95_INT);
- 
-// Blinky on receipt
-#define LED 13
+// GPIO5  -- SX1278's SCK
+// GPIO19 -- SX1278's MISO
+// GPIO27 -- SX1278's MOSI
+// GPIO18 -- SX1278's CS
+// GPIO14 -- SX1278's RESET
+// GPIO26 -- SX1278's IRQ(Interrupt Request)
+
+#define SS      18
+#define RST     14
+#define DI0     26
+#define BAND    433E6 // Change to 434.0 or other frequency, must match RX's freq!
+
+#define VBATPIN 36 //battery voltage check pin
+#define LED 25 //LED Pin
 
 //Returns the battery voltage as a float.
 float voltage(){
@@ -74,13 +46,15 @@ float voltage(){
 }
 
 void radioon(){
+  /*
+   * // I don't believe this is needed for this board/library (?)
   // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
  
-  while (!rf95.init()) {
+  while (!LoRa.init()) {
     Serial.println("LoRa radio init failed");
     while (1);
   }
@@ -102,27 +76,36 @@ void radioon(){
   rf95.setTxPower(23, false);
   Serial.println("Set power to 23.");
   Serial.print("Max packet length: "); Serial.println(RH_RF95_MAX_MESSAGE_LEN);
+*/
 }
 
 void radiooff(){
+  //arduino-LoRa library provides a sleep, and a idle mode (Sleep is what I beleive we need here) 
   // manual reset
-  digitalWrite(RFM95_RST, LOW);
+  //digitalWrite(RFM95_RST, LOW);
+  LoRa.sleep();
   delay(10);
 }
 
 void setup() 
 {
-  pinMode(LED, OUTPUT);     
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
-
+  pinMode(LED, OUTPUT);
+  
+Serial.begin(9600);
+Serial.setTimeout(10);
+  while (!Serial); //if just the the basic function, must connect to a computer
   delay(1000);
-  //while (!Serial);
-  Serial.begin(9600);
-  Serial.setTimeout(10);
-  delay(100);
- 
-  radioon();
+  
+  Serial.println("BEACON"); 
+  
+  SPI.begin(5,19,27,18);
+  LoRa.setPins(SS,RST,DI0);
+  
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  LoRa.enableCrc(); //encable CRC checking - off by default
   digitalWrite(LED, LOW);
 }
 
@@ -148,9 +131,9 @@ void beacon(){
   
   //Serial.println("Transmitting..."); // Send a message to rf95_server
   
-  char radiopacket[RH_RF95_MAX_MESSAGE_LEN];
+  char radiopacket[128];
   snprintf(radiopacket,
-           RH_RF95_MAX_MESSAGE_LEN,
+           128,
            "BEACON %s %s VCC=%d.%03d count=%d uptime=%ld",
            CALLSIGN,
            COMMENTS,
@@ -162,16 +145,19 @@ void beacon(){
   radiopacket[sizeof(radiopacket)] = 0;
   
   //Serial.println("Sending..."); delay(10);
-  rf95.send((uint8_t *)radiopacket, strlen((char*) radiopacket));
+  LoRa.beginPacket();
+  LoRa.print(radiopacket);
+  LoRa.endPacket();
+  //rf95.send((uint8_t *)radiopacket, strlen((char*) radiopacket));
  
   //Serial.println("Waiting for packet to complete..."); delay(10);
-  rf95.waitPacketSent();
+  //rf95.waitPacketSent();
   packetnum++;
 }
 
 void loop(){
   //Turn the radio on.
-  radioon();
+  //radioon();
   //Transmit a beacon once every five minutes.
   beacon();
   //Then turn the radio off to save power.
