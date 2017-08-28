65515de932b35527967905df048df3e85cb03b85
KD8BXP <kd8bxp@aol.com>
2017-08-28 18:03:43 -0400

    added deep sleep to esp32beacon.ino

--- arduino/esp32beacon/esp32beacon.ino
+++ arduino/esp32beacon/esp32beacon.ino
@@ -35,6 +35,9 @@
 #define VBATPIN 36 //battery voltage check pin
 #define LED 25 //LED Pin
 
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  300        /* Time ESP32 will go to sleep (in seconds) 5 minutes*/
+
 //Returns the battery voltage as a float.
 float voltage(){
 
@@ -46,37 +49,7 @@ float voltage(){
 }
 
 void radioon(){
-  /*
-   * // I don't believe this is needed for this board/library (?)
-  // manual reset
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
  
-  while (!LoRa.init()) {
-    Serial.println("LoRa radio init failed");
-    while (1);
-  }
-  Serial.println("LoRa radio init OK!");
- 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while (1);
-  }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-  
- 
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
- 
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
-  // you can set transmitter powers from 5 to 23 dBm:
-  rf95.setTxPower(23, false);
-  Serial.println("Set power to 23.");
-  Serial.print("Max packet length: "); Serial.println(RH_RF95_MAX_MESSAGE_LEN);
-*/
 }
 
 void radiooff(){
@@ -107,6 +80,7 @@ Serial.setTimeout(10);
   }
   LoRa.enableCrc(); //encable CRC checking - off by default
   digitalWrite(LED, LOW);
+  esp_deep_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 }
 
 
@@ -164,6 +138,7 @@ void loop(){
   radiooff();
   
   //Five minute delay between beacons.
-  delay(5*60000);
+  //delay(5*60000);
+  esp_deep_sleep_start();
 }
 
